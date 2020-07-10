d59e7ead6f4379a8d0aa2e43bfe1dcfd3f398b37
ChelseaDeB <54922401+ChelseaDeB@users.noreply.github.com>
2020-07-10 17:09:18 -0400

    Add files via upload

--- convertingUniprot-NCBI.py
+++ convertingUniprot-NCBI.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 10 10:31:05 2020
-
-@author: chelsea
-"""
 
 import urllib.parse 
 import urllib.request
@@ -12,11 +7,11 @@ import urllib.request
 url = 'https://www.uniprot.org/uploadlists/'
 
 params = {
-    'from' : 'ACC+ID',
+    'from' : 'ACC+ID', #ACC+ID = UniProtKB AC/ID
     'to' : 'P_REFSEQ_AC', #RefSeq Protein = NCBI protein 
                           # or P_ENTREZGENEID  NCBI gene
     'format': 'tab',
-    'query' : 'Q239Q2 Q236L2 A4VDZ5' #ACC+ID = UniProtKB AC/ID
+    'query' : 'Q239Q2 Q236L2 A4VDZ5' 
     }
 
 data = urllib.parse.urlencode(params)
@@ -26,3 +21,33 @@ with urllib.request.urlopen(req) as f:
     response = f.read()
     
     print (response.decode('utf-8'))
+    
+""" 
+    This function is used to convert a Uniprot Id into a NCBI protein 
+    ID. ONce called it will prompt the user to input the IDs they wish
+    to convert. The IDs will be converted and the NCBI IDs will be printed
+    to the screen
+    
+"""
+    
+def Convert():
+    print('input the Uniprot ID of the protein') 
+    queryString = input() #saves the inputed string 
+    params = {
+    'from' : 'ACC+ID', #ACC+ID = UniProtKB AC/ID
+    'to' : 'P_REFSEQ_AC', #RefSeq Protein = NCBI protein 
+                          # or P_ENTREZGENEID  NCBI gene
+    'format': 'tab',
+    'query' : queryString 
+    }
+    
+    data = urllib.parse.urlencode(params)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as f:
+        response = f.read()
+    
+        print (response.decode('utf-8'))
+
+Convert()
+    
